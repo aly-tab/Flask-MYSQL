d07ff5678ac522436d3119d6292458a66f5c593b
w-phil <wphillips892@gmail.com>
2021-07-17 11:47:38 -0400

    books

---
+++ Books/flask_app/controllers/books.py
@@ -0,0 +1,115 @@
+from flask_app import app
+from flask import render_template, request, redirect
+from flask_app.models.author import Author
+from flask_app.models.book import Book
+from flask_app.models.favorite import Favorite
+
+
+@app.route('/authors')          
+def index():
+    authors = Author.get_all()
+    print(authors)
+    return render_template('index.html', authors=authors)  
+
+@app.route('/books')          
+def books():
+    books = Book.get_all()
+    print(books)
+    return render_template('books.html', books=books) 
+
+@app.route('/authors/<int:id>')          
+def author_favorites(id):
+    data = {
+        "id" : id
+    }
+    author = Author.get_author_with_books(data)
+    print(author)
+
+    if author == 0:
+        authors = Author.get_all()
+        for author in authors:
+            if author.id == id:
+                author = author
+
+    books = Book.get_all()
+    print(books)
+
+    return render_template('author-favorites.html', author=author, books=books)  
+
+@app.route('/books/<int:id>')          
+def book_favorites(id):
+    data = {
+        "id" : id
+    }
+    book = Book.get_book_with_authors(data)
+    print(book)
+
+    if book == 0:
+        books = Book.get_all()
+        for book in books:
+            if book.id == id:
+                book = book
+
+    authors = Author.get_all() 
+    print(authors)
+
+    return render_template('book-favorites.html', book=book, authors=authors) 
+
+@app.route('/authors/addauthor', methods=['POST'])          
+def addauthor():
+    print(request.form)
+    data = {
+        "name" : request.form['name'],
+    }
+    author = Author.save(data)
+    print(author)
+
+    return redirect('/authors')
+
+@app.route('/books/addbook', methods=['POST'])          
+def addbook():
+    print(request.form)
+    data = {
+        "title" : request.form['title'],
+        "pages" : request.form['pages']
+
+    }
+
+    book = Book.save(data)
+    print(book)
+
+
+    return redirect('/books')
+
+@app.route('/authors/addauthorfavorite', methods=['POST'])          
+def addauthorfavorite():
+    print(request.form)
+
+    author_id = request.form["authorid"]
+
+    data = {
+        "author_id" : request.form['authorid'],
+        "book_id" : request.form['bookid']
+
+    }
+
+    favorite = Favorite.save(data)
+    print(favorite)
+
+    return redirect(f'/authors/{author_id}')
+
+@app.route('/books/addbookfavorite', methods=['POST'])  
+def addbookfavorite():
+    print(request.form)
+    book_id = request.form['bookid']
+
+    data = {
+        "author_id" : request.form['authorid'],
+        "book_id" : request.form['bookid']
+
+    }
+
+    favorite = Favorite.save(data)
+    print(favorite)
+
+    return redirect(f'/books/{book_id}')
---
+++ Books/flask_app/models/author.py
@@ -0,0 +1,49 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import book
+
+class Author:
+	def __init__( self , data ):
+		self.id = data['id']
+		self.name = data['name']
+		self.created_at = data['created_at']
+		self.updated_at = data['updated_at']
+		
+		self.books = []  		       
+
+	@classmethod
+	def get_all(cls):
+		query = "SELECT * FROM authors;"
+
+		results = connectToMySQL('books_schema').query_db(query)
+		authors = []
+		for author in results:
+			authors.append( cls(author) )
+		return authors
+
+	@classmethod
+	def save(cls, data ):
+		query = "INSERT INTO authors ( name, created_at, updated_at ) VALUES ( %(name)s , NOW() , NOW() );"
+		return connectToMySQL('books_schema').query_db( query, data )
+
+#=============================================================================================================#
+
+	@classmethod
+	def get_author_with_books(cls, data):
+		query = "SELECT * FROM authors LEFT JOIN favorites ON authors.id = favorites.author_id LEFT JOIN books ON books.id = favorites.book_id WHERE author_id = %(id)s;"
+		results = connectToMySQL('books_schema').query_db( query , data )
+
+		if len(results) == 0:
+			return 0	
+			
+		else:
+			author = cls(results[0])
+			for row_from_db in results:
+				books_data = {
+					"id" : row_from_db["books.id"],
+					"title" : row_from_db["title"],
+					"num_of_pages" : row_from_db["num_of_pages"],
+					"created_at" : row_from_db["books.created_at"],
+					"updated_at" : row_from_db["books.updated_at"]		
+				}
+				author.books.append(book.Book( books_data) )
+			return author
